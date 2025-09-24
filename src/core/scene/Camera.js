ee36a35bfa7bb47cf32ef7c50540e880bea97352
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-24 18:37:20 +0700

    Update Camera.js

--- src/core/scene/Camera.js
+++ src/core/scene/Camera.js
@@ -2,25 +2,43 @@ import { Object3D } from './Object3D.js';
 import { Matrix4 } from '../math/Matrix4.js';
 
 export class Camera extends Object3D {
-  constructor(fov=60, aspect=1, near=0.1, far=1000){
+  constructor(fov=60, aspect=1, near=0.1, far=1000) {
     super();
+    this.type = 'Camera';
     this.fov = fov;
     this.aspect = aspect;
     this.near = near;
     this.far = far;
 
     this.projectionMatrix = new Matrix4();
-    this.updateProjection(aspect);
+    this.viewMatrix = new Matrix4();
+    this.matrixWorldInverse = new Matrix4();
+
+    this.updateProjection();
   }
 
-  updateProjection(aspect=this.aspect){
+  /** Update projection matrix (perspective) */
+  updateProjection() {
+    const f = 1.0 / Math.tan((this.fov * Math.PI) / 360);
+    const nf = 1 / (this.near - this.far);
+    const e = this.projectionMatrix.elements;
+
+    e[0] = f / this.aspect; e[1] = 0; e[2] = 0; e[3] = 0;
+    e[4] = 0; e[5] = f; e[6] = 0; e[7] = 0;
+    e[8] = 0; e[9] = 0; e[10] = (this.far + this.near) * nf; e[11] = -1;
+    e[12] = 0; e[13] = 0; e[14] = (2 * this.far * this.near) * nf; e[15] = 0;
+  }
+
+  /** Update view matrix based on world matrix */
+  updateViewMatrix() {
+    this.updateMatrix();
+    this.matrixWorldInverse = this.matrix.clone().invert();
+    this.viewMatrix = this.matrixWorldInverse.clone();
+  }
+
+  /** Set aspect ratio and recalc projection */
+  setAspect(aspect) {
     this.aspect = aspect;
-    this.projectionMatrix.makePerspective(this.fov,this.aspect,this.near,this.far);
-  }
-
-  lookAt(target){
-    const m = new Matrix4().lookAt(this.position, target, {x:0,y:1,z:0});
-    // extract rotation from matrix
-    this.rotation.setFromEuler(Math.atan2(m.elements[6], m.elements[10]), Math.asin(-m.elements[2]), Math.atan2(m.elements[1], m.elements[0]));
+    this.updateProjection();
   }
 }
