497b06de3cfb6bb4c72117eb21d8812c264a667a
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-24 19:55:42 +0700

    Update Vector4.js

--- src/core/math/Vector4.js
+++ src/core/math/Vector4.js
@@ -1,3 +1,4 @@
+// src/core/math/Vector4.js
 export class Vector4 {
   constructor(x=0, y=0, z=0, w=1) {
     this.x = x; this.y = y; this.z = z; this.w = w;
@@ -13,22 +14,21 @@ export class Vector4 {
   subtract(v){ return new Vector4(this.x-v.x, this.y-v.y, this.z-v.z, this.w-v.w); }
   subtractInPlace(v){ this.x-=v.x; this.y-=v.y; this.z-=v.z; this.w-=v.w; return this; }
 
-  multiplyScalar(s){ return new Vector4(this.x*s,this.y*s,this.z*s,this.w*s); }
-  multiplyComponents(v){ return new Vector4(this.x*v.x,this.y*v.y,this.z*v.z,this.w*v.w); }
-  multiplyComponentsInPlace(v){ this.x*=v.x; this.y*=v.y; this.z*=v.z; this.w*=v.w; return this; }
+  multiplyScalar(s){ return new Vector4(this.x*s, this.y*s, this.z*s, this.w*s); }
+  multiplyInPlaceScalar(s){ this.x*=s; this.y*=s; this.z*=s; this.w*=s; return this; }
 
   divideScalar(s){ return s!==0? this.multiplyScalar(1/s) : this.clone(); }
 
-  dot(v){ return this.x*v.x+this.y*v.y+this.z*v.z+this.w*v.w; }
+  dot(v){ return this.x*v.x + this.y*v.y + this.z*v.z + this.w*v.w; }
 
-  length(){ return Math.hypot(this.x,this.y,this.z,this.w); }
+  length(){ return Math.hypot(this.x, this.y, this.z, this.w); }
 
   normalize(){ const l=this.length()||1; this.x/=l; this.y/=l; this.z/=l; this.w/=l; return this; }
 
-  toArray(){ return [this.x,this.y,this.z,this.w]; }
+  toArray(){ return [this.x, this.y, this.z, this.w]; }
   fromArray(arr){ this.x=arr[0]||0; this.y=arr[1]||0; this.z=arr[2]||0; this.w=arr[3]!==undefined?arr[3]:1; return this; }
 
   lerp(v,t){ this.x+=(v.x-this.x)*t; this.y+=(v.y-this.y)*t; this.z+=(v.z-this.z)*t; this.w+=(v.w-this.w)*t; return this; }
 
-  toString(){ return `Vector4(${this.x},${this.y},${this.z},${this.w})`; }
+  toString(){ return `Vector4(${this.x}, ${this.y}, ${this.z}, ${this.w})`; }
 }
