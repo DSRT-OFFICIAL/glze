f4c85914f3dc775afe40e8100c14751b21361402
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-24 19:12:09 +0700

    Update WebGLRenderer.js

--- src/core/render/WebGLRenderer.js
+++ src/core/render/WebGLRenderer.js
@@ -1,101 +1,109 @@
 import { Matrix3 } from '../math/Matrix3.js';
 
 export class WebGLRenderer {
-  constructor(canvas){
+  constructor(canvas) {
     this.canvas = canvas;
     this.gl = canvas.getContext('webgl2') || canvas.getContext('webgl');
-    if(!this.gl) throw new Error('WebGL not supported');
+    if (!this.gl) throw new Error('WebGL not supported');
 
-    this.clearColor = [0,0,0,1];
-    this.gl.enable(this.gl.DEPTH_TEST);
+    this.clearColor = [0, 0, 0, 1];
+    this.autoClear = true;
 
-    this.programCache = new Map();
+    this.meshes = new Set(); // semua mesh untuk draw
+    this.defaultShader = this.createDefaultShader();
   }
 
-  setClearColor(r,g,b,a=1){
-    this.clearColor = [r,g,b,a];
-    this.gl.clearColor(r,g,b,a);
+  setClearColor(r, g, b, a = 1) {
+    this.clearColor = [r, g, b, a];
   }
 
-  render(scene, camera){
+  clear() {
     const gl = this.gl;
-    gl.viewport(0,0,this.canvas.width,this.canvas.height);
+    gl.clearColor(...this.clearColor);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    scene.updateWorldMatrix();
-
-    for(const child of scene.children){
-      this.renderObject(child, camera);
-    }
   }
 
-  renderObject(object, camera){
-    if(!object.visible) return;
+  setSize(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.gl.viewport(0, 0, width, height);
+  }
 
-    // assume object has geometry + material
-    if(object.geometry && object.material){
+  /** Register mesh supaya bisa dirender */
+  registerMesh(mesh) {
+    this.meshes.add(mesh);
+    mesh.initBuffers(this.gl);
+  }
+
+  /** Render scene dengan camera */
+  render(scene, camera) {
+    if (this.autoClear) this.clear();
+
+    camera.updateWorldMatrix();
+    camera.updateViewMatrix();
+
+    scene.updateMatrixWorld();
+
+    // traverse mesh
+    this.meshes.forEach(mesh => {
+      if (!mesh.visible) return;
+
       const gl = this.gl;
-      const shader = this.getProgram(object.material);
+      const shader = mesh.material?.shader || this.defaultShader;
       gl.useProgram(shader.program);
 
       // set uniforms
-      const uModel = gl.getUniformLocation(shader.program,'uModel');
-      const uView = gl.getUniformLocation(shader.program,'uView');
-      const uProjection = gl.getUniformLocation(shader.program,'uProjection');
-
-      gl.uniformMatrix4fv(uModel,false,object.worldMatrix.elements);
-      gl.uniformMatrix4fv(uView,false,camera.worldMatrix.elements);
-      gl.uniformMatrix4fv(uProjection,false,camera.projectionMatrix.elements);
+      gl.uniformMatrix4fv(shader.uProjection, false, camera.projectionMatrix.elements);
+      gl.uniformMatrix4fv(shader.uView, false, camera.viewMatrix.elements);
+      gl.uniformMatrix4fv(shader.uModel, false, mesh.worldMatrix.elements);
 
       // normal matrix
-      if(object.geometry.normalBuffer){
-        const normalMatrix = new Matrix3().getNormalMatrix(object.worldMatrix);
-        const uNormal = gl.getUniformLocation(shader.program,'uNormalMatrix');
-        gl.uniformMatrix3fv(uNormal,false,normalMatrix.elements);
+      const normalMatrix = new Matrix3().getNormalMatrix(mesh.worldMatrix);
+      gl.uniformMatrix3fv(shader.uNormalMatrix, false, normalMatrix.elements);
+
+      mesh.draw(gl, shader);
+    });
+  }
+
+  /** Simple default shader */
+  createDefaultShader() {
+    const gl = this.gl;
+    const vs = `
+      attribute vec3 position;
+      uniform mat4 uModel;
+      uniform mat4 uView;
+      uniform mat4 uProjection;
+      void main() {
+        gl_Position = uProjection * uView * uModel * vec4(position,1.0);
       }
+    `;
+    const fs = `
+      precision mediump float;
+      uniform vec4 uColor;
+      void main() {
+        gl_FragColor = uColor;
+      }
+    `;
+    const vert = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vert, vs);
+    gl.compileShader(vert);
 
-      // bind buffers & draw
-      object.geometry.bind(gl, shader);
-      object.geometry.draw(gl);
-    }
+    const frag = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(frag, fs);
+    gl.compileShader(frag);
 
-    // render children recursively
-    for(const c of object.children){
-      this.renderObject(c, camera);
-    }
-  }
-
-  getProgram(material){
-    if(this.programCache.has(material)){
-      return this.programCache.get(material);
-    }
-
-    const gl = this.gl;
-    const vertexShader = this.createShader(gl.VERTEX_SHADER, material.vertexShader);
-    const fragmentShader = this.createShader(gl.FRAGMENT_SHADER, material.fragmentShader);
     const program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
+    gl.attachShader(program, vert);
+    gl.attachShader(program, frag);
     gl.linkProgram(program);
-    if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
-      console.error(gl.getProgramInfoLog(program));
-      return null;
-    }
 
-    const shaderObj = { program, vertexShader, fragmentShader };
-    this.programCache.set(material, shaderObj);
-    return shaderObj;
-  }
-
-  createShader(type, source){
-    const gl = this.gl;
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if(!gl.getShaderParameter(shader,gl.COMPILE_STATUS)){
-      console.error(gl.getShaderInfoLog(shader));
-      return null;
-    }
-    return shader;
+    return {
+      program,
+      uProjection: gl.getUniformLocation(program, 'uProjection'),
+      uView: gl.getUniformLocation(program, 'uView'),
+      uModel: gl.getUniformLocation(program, 'uModel'),
+      uNormalMatrix: gl.getUniformLocation(program, 'uNormalMatrix'),
+      uColor: gl.getUniformLocation(program, 'uColor')
+    };
   }
 }
