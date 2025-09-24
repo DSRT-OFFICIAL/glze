3439344c3af2c1a199be9dbc0f6d994550efa99f
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-25 00:17:35 +0700

    Update Mesh.js

--- src/core/Mesh.js
+++ src/core/Mesh.js
@@ -1,9 +1,26 @@
-import { Object3D } from './scene/Object3D.js';
+import { Object3D } from './Object3D.js';
+import { Material } from './Material.js';
+import { Geometry } from '../geometry/Geometry.js';
 
 export class Mesh extends Object3D {
-  constructor(geometry, material){
+  constructor(geometry = new Geometry(), material = new Material()) {
     super();
     this.geometry = geometry;
     this.material = material;
+    this.type = 'Mesh';
   }
-}
+
+  updateMatrix() {
+    this.matrix.compose(this.position, this.quaternion, this.scale);
+    this.matrixWorldNeedsUpdate = true;
+  }
+
+  setMaterial(material) {
+    this.material = material;
+    return this;
+  }
+
+  clone() {
+    return new Mesh(this.geometry.clone(), this.material.clone());
+  }
+  }
