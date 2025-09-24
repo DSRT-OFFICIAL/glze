32fdc8af231aa1e5ae52b6b887b635a250e243db
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-24 20:48:51 +0700

    Update Camera.js

--- src/core/scene/Camera.js
+++ src/core/scene/Camera.js
@@ -1,44 +1,41 @@
-import { Object3D } from './Object3D.js';
-import { Matrix4 } from '../math/Matrix4.js';
+import { Vector3 } from './Vector3.js';
+import { Matrix4 } from './Matrix4.js';
 
-export class Camera extends Object3D {
-  constructor(fov=60, aspect=1, near=0.1, far=1000) {
-    super();
-    this.type = 'Camera';
-    this.fov = fov;
-    this.aspect = aspect;
-    this.near = near;
-    this.far = far;
+export class Camera {
+    constructor(fov=60, aspect=1, near=0.1, far=1000) {
+        this.position = new Vector3();
+        this.rotation = new Vector3();
+        this.up = new Vector3(0,1,0);
 
-    this.projectionMatrix = new Matrix4();
-    this.viewMatrix = new Matrix4();
-    this.matrixWorldInverse = new Matrix4();
+        this.fov = fov;
+        this.aspect = aspect;
+        this.near = near;
+        this.far = far;
 
-    this.updateProjection();
-  }
+        this.matrix = new Matrix4();
+        this.matrixWorld = new Matrix4();
+        this.projectionMatrix = new Matrix4();
 
-  /** Update projection matrix (perspective) */
-  updateProjection() {
-    const f = 1.0 / Math.tan((this.fov * Math.PI) / 360);
-    const nf = 1 / (this.near - this.far);
-    const e = this.projectionMatrix.elements;
+        this.viewport = { x:0, y:0, width:1, height:1 };
+        this.renderTarget = null;
+        this.controls = null; // optional OrbitControls or custom
+    }
 
-    e[0] = f / this.aspect; e[1] = 0; e[2] = 0; e[3] = 0;
-    e[4] = 0; e[5] = f; e[6] = 0; e[7] = 0;
-    e[8] = 0; e[9] = 0; e[10] = (this.far + this.near) * nf; e[11] = -1;
-    e[12] = 0; e[13] = 0; e[14] = (2 * this.far * this.near) * nf; e[15] = 0;
-  }
+    lookAt(target) {
+        // compute rotation from current position to target
+    }
 
-  /** Update view matrix based on world matrix */
-  updateViewMatrix() {
-    this.updateMatrix();
-    this.matrixWorldInverse = this.matrix.clone().invert();
-    this.viewMatrix = this.matrixWorldInverse.clone();
-  }
+    updateProjectionMatrix() {
+        const f = 1.0 / Math.tan((this.fov * 0.5) * (Math.PI / 180));
+        this.projectionMatrix.elements = [
+            f / this.aspect, 0, 0, 0,
+            0, f, 0, 0,
+            0, 0, (this.far+this.near)/(this.near-this.far), (2*this.far*this.near)/(this.near-this.far),
+            0, 0, -1, 0
+        ];
+    }
 
-  /** Set aspect ratio and recalc projection */
-  setAspect(aspect) {
-    this.aspect = aspect;
-    this.updateProjection();
-  }
+    updateMatrixWorld(force=false) {
+        // compute world matrix from position, rotation
+    }
 }
