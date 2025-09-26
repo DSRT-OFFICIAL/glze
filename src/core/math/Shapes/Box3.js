2f3dde0733d132fb85367d74bf047a7b3bb3d0fa
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:29:59 +0700

    Update Box3.js

--- src/core/math/Shapes/Box3.js
+++ src/core/math/Shapes/Box3.js
@@ -1,5 +1,5 @@
 // core/math/Shapes/Box3.js
-import { Vector3 } from '../primitives/Vector3.js';
+import { Vector3 } from '../Vector3.js';
 
 class Box3 {
     constructor(min = new Vector3(+Infinity, +Infinity, +Infinity), max = new Vector3(-Infinity, -Infinity, -Infinity)) {
@@ -13,27 +13,14 @@ class Box3 {
         return this;
     }
 
-    setFromPoints(points) {
-        this.min.set(+Infinity, +Infinity, +Infinity);
-        this.max.set(-Infinity, -Infinity, -Infinity);
-        points.forEach(p => this.expandByPoint(p));
-        return this;
+    clone() {
+        return new Box3(this.min.clone(), this.max.clone());
     }
 
-    expandByPoint(point) {
-        this.min.x = Math.min(this.min.x, point.x);
-        this.min.y = Math.min(this.min.y, point.y);
-        this.min.z = Math.min(this.min.z, point.z);
-        this.max.x = Math.max(this.max.x, point.x);
-        this.max.y = Math.max(this.max.y, point.y);
-        this.max.z = Math.max(this.max.z, point.z);
+    copy(box) {
+        this.min.copy(box.min);
+        this.max.copy(box.max);
         return this;
-    }
-
-    containsPoint(point) {
-        return point.x >= this.min.x && point.x <= this.max.x &&
-               point.y >= this.min.y && point.y <= this.max.y &&
-               point.z >= this.min.z && point.z <= this.max.z;
     }
 
     getCenter(target = new Vector3()) {
@@ -44,18 +31,20 @@ class Box3 {
         return target.subVectors(this.max, this.min);
     }
 
-    isEmpty() {
-        return this.max.x < this.min.x || this.max.y < this.min.y || this.max.z < this.min.z;
+    containsPoint(point) {
+        return point.x >= this.min.x && point.x <= this.max.x &&
+               point.y >= this.min.y && point.y <= this.max.y &&
+               point.z >= this.min.z && point.z <= this.max.z;
     }
 
-    clone() {
-        return new Box3().copy(this);
-    }
-
-    copy(box) {
-        this.min.copy(box.min);
-        this.max.copy(box.max);
+    expandByPoint(point) {
+        this.min.min(point);
+        this.max.max(point);
         return this;
+    }
+
+    toString() {
+        return `Box3(min: ${this.min.toArray()}, max: ${this.max.toArray()})`;
     }
 }
 
