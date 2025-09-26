a66bc484f620041c15502f5c98cdb1b781f33d56
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 21:57:47 +0700

    Update index.js

--- src/core/math/Interpolant/index.js
+++ src/core/math/Interpolant/index.js
@@ -1,1 +1,5 @@
-
+export { Interpolant } from './Interpolant.js';
+export { LinearInterpolant } from './LinearInterpolant.js';
+export { CubicInterpolant } from './CubicInterpolant.js';
+export { DiscreteInterpolant } from './DiscreteInterpolant.js';
+export { QuaternionLinearInterpolant } from './QuaternionLinearInterpolant.js';
