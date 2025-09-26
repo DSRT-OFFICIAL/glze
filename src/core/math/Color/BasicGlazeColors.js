f138e049485fb1560646d603d9c0aea0d9c6b2b0
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 17:21:42 +0700

    Create BasicGlazeColors.js

---
+++ src/core/math/Color/BasicGlazeColors.js
@@ -0,0 +1,17 @@
+import { Color } from './Color.js';
+
+export const BasicColors = {
+    black: new Color(0x000000),
+    white: new Color(0xffffff),
+    red: new Color(0xff0000),
+    green: new Color(0x00ff00),
+    blue: new Color(0x0000ff),
+    cyan: new Color(0x00ffff),
+    magenta: new Color(0xff00ff),
+    yellow: new Color(0xffff00),
+    orange: new Color(0xff8800),
+    brown: new Color(0x885522),
+    purple: new Color(0x800080),
+    pink: new Color(0xffc0cb),
+    gray: new Color(0x808080)
+};
