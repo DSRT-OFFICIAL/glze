75960e4b29b9670a5bb5d4c91987b47dfa6df618
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:56:27 +0700

    Update Sphere.js

--- src/core/math/Shapes/Sphere.js
+++ src/core/math/Shapes/Sphere.js
@@ -1,20 +1,16 @@
-// core/math/Shapes/Sphere.js
-import { Vector3 } from '../Vector3.js';
+// glze/src/core/math/Shapes/Sphere.js
+import { Vector3 } from '../Primitives/Vector3.js';
 
-class Sphere {
-    constructor(center = new Vector3(), radius = 0) {
+export class Sphere {
+    constructor(center = new Vector3(), radius = 1) {
         this.center = center;
         this.radius = radius;
     }
 
     set(center, radius) {
-        this.center.copy(center);
+        this.center = center;
         this.radius = radius;
         return this;
-    }
-
-    clone() {
-        return new Sphere(this.center.clone(), this.radius);
     }
 
     copy(sphere) {
@@ -23,41 +19,15 @@ class Sphere {
         return this;
     }
 
+    clone() {
+        return new Sphere(this.center.clone(), this.radius);
+    }
+
     containsPoint(point) {
-        return point.distanceToSquared(this.center) <= (this.radius * this.radius);
-    }
-
-    distanceToPoint(point) {
-        return point.distanceTo(this.center) - this.radius;
-    }
-
-    intersectsSphere(sphere) {
-        const radiusSum = this.radius + sphere.radius;
-        return this.center.distanceToSquared(sphere.center) <= (radiusSum * radiusSum);
-    }
-
-    getBoundingBox(targetMin, targetMax) {
-        targetMin.set(
-            this.center.x - this.radius,
-            this.center.y - this.radius,
-            this.center.z - this.radius
-        );
-        targetMax.set(
-            this.center.x + this.radius,
-            this.center.y + this.radius,
-            this.center.z + this.radius
-        );
-        return { min: targetMin, max: targetMax };
-    }
-
-    translate(offset) {
-        this.center.add(offset);
-        return this;
+        return this.center.distanceTo(point) <= this.radius;
     }
 
     toString() {
-        return `Sphere(center: ${this.center.toArray()}, radius: ${this.radius})`;
+        return `Sphere(center=${this.center.toString()}, radius=${this.radius})`;
     }
 }
-
-export { Sphere };
