813463f552b6b1b07e7d8a73831cbf9b5ce12fbb
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:14:50 +0700

    Update index.js

--- src/core/math/Shapes/index.js
+++ src/core/math/Shapes/index.js
@@ -1,1 +1,12 @@
+// glze/src/core/math/Shapes/index.js
 
+export { Box2 } from './Box2.js';
+export { Box3 } from './Box3.js';
+export { Sphere } from './Sphere.js';
+export { Plane } from './Plane.js';
+export { Triangle } from './Triangle.js';
+export { Frustum } from './Frustum.js';
+export { FrustumArray } from './FrustumArray.js';
+export { Spherical } from './Spherical.js';
+export { Cylindrical } from './Cylindrical.js';
+export { SphericalHarmonics3 } from './SphericalHarmonics3.js';
