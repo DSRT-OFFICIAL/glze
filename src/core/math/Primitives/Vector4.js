9dd3e729e96e1e8d37ee3fd09246ccb2eec2ed25
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 18:40:19 +0700

    Update Vector4.js

--- src/core/math/Primitives/Vector4.js
+++ src/core/math/Primitives/Vector4.js
@@ -1,1 +1,72 @@
+export class Vector4 {
+    constructor(x = 0, y = 0, z = 0, w = 1) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
 
+    set(x, y, z, w) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+        return this;
+    }
+
+    clone() {
+        return new Vector4(this.x, this.y, this.z, this.w);
+    }
+
+    copy(v) {
+        this.x = v.x;
+        this.y = v.y;
+        this.z = v.z;
+        this.w = v.w;
+        return this;
+    }
+
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        this.w += v.w;
+        return this;
+    }
+
+    sub(v) {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.z -= v.z;
+        this.w -= v.w;
+        return this;
+    }
+
+    multiplyScalar(scalar) {
+        this.x *= scalar;
+        this.y *= scalar;
+        this.z *= scalar;
+        this.w *= scalar;
+        return this;
+    }
+
+    divideScalar(scalar) {
+        return this.multiplyScalar(1 / scalar);
+    }
+
+    dot(v) {
+        return this.x * v.x + this.y * v.y + this.z * v.z + this.w * v.w;
+    }
+
+    toArray() {
+        return [this.x, this.y, this.z, this.w];
+    }
+
+    fromArray(arr) {
+        this.x = arr[0];
+        this.y = arr[1];
+        this.z = arr[2];
+        this.w = arr[3];
+        return this;
+    }
+}
