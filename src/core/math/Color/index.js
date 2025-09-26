4859856fad98b9989a06c827c128f09ad9c71a12
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 17:27:31 +0700

    Update index.js

--- src/core/math/Color/index.js
+++ src/core/math/Color/index.js
@@ -1,36 +1,10 @@
-import { Color } from './Color.js';
-import { BasicColors } from './BasicGlazeColors.js';
-import { ExtendedColors } from './ExtendedGlazeColors.js';
+// glze/src/core/math/Color/index.js
 
-// Utility functions
-function lerpColors(c1, c2, alpha) {
-    return c1.clone().lerp(c2, alpha);
-}
+// Core Color class
+export { Color } from './Color.js';
 
-function mixColors(c1, c2, alpha) {
-    return c1.clone().mix(c2, alpha);
-}
+// Basic color palette
+export { BasicColors } from './BasicGlazeColors.js';
 
-function addColors(c1, c2) {
-    return c1.clone().add(c2);
-}
-
-function multiplyColors(c1, c2) {
-    return c1.clone().multiply(c2);
-}
-
-function invertColor(c) {
-    return c.clone().invert();
-}
-
-// Export everything
-export {
-    Color,
-    BasicColors,
-    ExtendedColors,
-    lerpColors,
-    mixColors,
-    addColors,
-    multiplyColors,
-    invertColor
-};
+// Extended color palette
+export { ExtendedColors } from './ExtendedGlazeColors.js';
