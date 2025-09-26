37e81a873b8358522ac7150afeca821ca77bb601
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 21:55:11 +0700

    Update DiscreteInterpolant.js

--- src/core/math/Interpolant/DiscreteInterpolant.js
+++ src/core/math/Interpolant/DiscreteInterpolant.js
@@ -1,1 +1,23 @@
+// src/core/math/Interpolant/DiscreteInterpolant.js
+import { Interpolant } from './Interpolant.js';
 
+/**
+ * DiscreteInterpolant returns the left sample value (step / hold).
+ */
+class DiscreteInterpolant extends Interpolant {
+
+	/**
+	 * For discrete interpolation we just copy the left sample.
+	 *
+	 * @param {number} i1
+	 * @return {TypedArray}
+	 */
+	interpolate_( i1 /*, t0, t, t1 */ ) {
+
+		return this.copySampleValue_( i1 - 1 );
+
+	}
+
+}
+
+export { DiscreteInterpolant }; 
