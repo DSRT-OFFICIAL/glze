6bea81c7f1911ee6fed49d6cc72a5d9fac4d6dea
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:19:39 +0700

    Update Box3.js

--- src/core/math/Shapes/Box3.js
+++ src/core/math/Shapes/Box3.js
@@ -1,1 +1,22 @@
+export class Box3 {
+  constructor(min = new Vector3(), max = new Vector3()) {
+    this.min = min;
+    this.max = max;
+  }
 
+  set(min, max) {
+    this.min.copy(min);
+    this.max.copy(max);
+    return this;
+  }
+
+  clone() {
+    return new Box3(this.min.clone(), this.max.clone());
+  }
+
+  containsPoint(point) {
+    return point.x >= this.min.x && point.x <= this.max.x &&
+           point.y >= this.min.y && point.y <= this.max.y &&
+           point.z >= this.min.z && point.z <= this.max.z;
+  }
+}
