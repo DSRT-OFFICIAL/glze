e0a0764d569bf9f08a70f141c330fa77885a082f
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:01:21 +0700

    Update Plane.js

--- src/core/math/Shapes/Plane.js
+++ src/core/math/Shapes/Plane.js
@@ -1,20 +1,49 @@
-export class Plane {
-  constructor(normal = new Vector3(1, 0, 0), constant = 0) {
-    this.normal = normal;
-    this.constant = constant;
-  }
+// core/math/Shapes/Plane.js
+import { Vector3 } from '../primitives/Vector3.js';
 
-  set(normal, constant) {
-    this.normal.copy(normal);
-    this.constant = constant;
-    return this;
-  }
+class Plane {
+    constructor(normal = new Vector3(1,0,0), constant = 0) {
+        this.normal = normal;
+        this.constant = constant;
+    }
 
-  clone() {
-    return new Plane(this.normal.clone(), this.constant);
-  }
+    set(normal, constant) {
+        this.normal.copy(normal);
+        this.constant = constant;
+        return this;
+    }
 
-  distanceToPoint(point) {
-    return this.normal.dot(point) + this.constant;
-  }
+    setFromNormalAndPoint(normal, point) {
+        this.normal.copy(normal);
+        this.constant = - point.dot(this.normal);
+        return this;
+    }
+
+    distanceToPoint(point) {
+        return this.normal.dot(point) + this.constant;
+    }
+
+    projectPoint(point, target) {
+        const distance = this.distanceToPoint(point);
+        return target.copy(this.normal).multiplyScalar(-distance).add(point);
+    }
+
+    normalize() {
+        const invLength = 1.0 / this.normal.length();
+        this.normal.multiplyScalar(invLength);
+        this.constant *= invLength;
+        return this;
+    }
+
+    clone() {
+        return new Plane().copy(this);
+    }
+
+    copy(plane) {
+        this.normal.copy(plane.normal);
+        this.constant = plane.constant;
+        return this;
+    }
 }
+
+export { Plane };
