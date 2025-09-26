adde94f09de8ae5b159121e084d033dd90c70268
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 21:24:32 +0700

    Update Matrix2.js

--- src/core/math/Primitives/Matrix2.js
+++ src/core/math/Primitives/Matrix2.js
@@ -1,37 +1,41 @@
 // glze/src/core/math/Matrix2.js
 export class Matrix2 {
-    constructor(
-        a = 1, b = 0,
-        c = 0, d = 1
-    ) {
-        this.elements = [a, b, c, d];
+    constructor() {
+        this.elements = [1,0,0,1];
     }
 
-    set(a, b, c, d) {
-        this.elements[0] = a;
-        this.elements[1] = b;
-        this.elements[2] = c;
-        this.elements[3] = d;
+    set(n11, n12, n21, n22) {
+        const te = this.elements;
+        te[0] = n11; te[1] = n12;
+        te[2] = n21; te[3] = n22;
         return this;
     }
 
     identity() {
-        return this.set(1, 0, 0, 1);
+        return this.set(1,0,0,1);
     }
 
-    multiply(m) {
-        const [a1, b1, c1, d1] = this.elements;
-        const [a2, b2, c2, d2] = m.elements;
-
-        this.elements[0] = a1 * a2 + b1 * c2;
-        this.elements[1] = a1 * b2 + b1 * d2;
-        this.elements[2] = c1 * a2 + d1 * c2;
-        this.elements[3] = c1 * b2 + d1 * d2;
-
+    copy(m) {
+        const te = this.elements;
+        const me = m.elements;
+        te[0] = me[0]; te[1] = me[1];
+        te[2] = me[2]; te[3] = me[3];
         return this;
     }
 
-    clone() {
-        return new Matrix2(...this.elements);
+    multiply(m) {
+        const ae = this.elements;
+        const be = m.elements;
+        const a11 = ae[0], a12 = ae[1];
+        const a21 = ae[2], a22 = ae[3];
+        const b11 = be[0], b12 = be[1];
+        const b21 = be[2], b22 = be[3];
+
+        ae[0] = a11*b11 + a12*b21;
+        ae[1] = a11*b12 + a12*b22;
+        ae[2] = a21*b11 + a22*b21;
+        ae[3] = a21*b12 + a22*b22;
+
+        return this;
     }
 }
