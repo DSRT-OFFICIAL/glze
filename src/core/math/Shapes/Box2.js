fe61283726bd61fc7ffe555d1d0062572033fb7a
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:19:55 +0700

    Update Box2.js

--- src/core/math/Shapes/Box2.js
+++ src/core/math/Shapes/Box2.js
@@ -1,1 +1,21 @@
+export class Box2 {
+  constructor(min = new Vector2(), max = new Vector2()) {
+    this.min = min;
+    this.max = max;
+  }
 
+  set(min, max) {
+    this.min.copy(min);
+    this.max.copy(max);
+    return this;
+  }
+
+  clone() {
+    return new Box2(this.min.clone(), this.max.clone());
+  }
+
+  containsPoint(point) {
+    return point.x >= this.min.x && point.x <= this.max.x &&
+           point.y >= this.min.y && point.y <= this.max.y;
+  }
+}
