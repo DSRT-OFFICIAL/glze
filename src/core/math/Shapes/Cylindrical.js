32ca3d081cbff942dba993a9dd83bdd0294e86cc
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:16:55 +0700

    Update Cylindrical.js

--- src/core/math/Shapes/Cylindrical.js
+++ src/core/math/Shapes/Cylindrical.js
@@ -1,1 +1,18 @@
+export class Cylindrical {
+  constructor(radius = 1, theta = 0, y = 0) {
+    this.radius = radius;
+    this.theta = theta;
+    this.y = y;
+  }
 
+  set(radius, theta, y) {
+    this.radius = radius;
+    this.theta = theta;
+    this.y = y;
+    return this;
+  }
+
+  clone() {
+    return new Cylindrical(this.radius, this.theta, this.y);
+  }
+}
