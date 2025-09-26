3907cd45cca723c5d0296aa43ae124768f20adfc
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:29:37 +0700

    Update Cylindrical.js

--- src/core/math/Shapes/Cylindrical.js
+++ src/core/math/Shapes/Cylindrical.js
@@ -1,8 +1,10 @@
 // core/math/Shapes/Cylindrical.js
+import { Vector3 } from '../Vector3.js';
+
 class Cylindrical {
     constructor(radius = 1, theta = 0, y = 0) {
         this.radius = radius;
-        this.theta = theta; // angle around y-axis
+        this.theta = theta;
         this.y = y;
     }
 
@@ -14,14 +16,32 @@ class Cylindrical {
     }
 
     clone() {
-        return new Cylindrical().copy(this);
+        return new Cylindrical(this.radius, this.theta, this.y);
     }
 
-    copy(cylindrical) {
-        this.radius = cylindrical.radius;
-        this.theta = cylindrical.theta;
-        this.y = cylindrical.y;
+    copy(cyl) {
+        this.radius = cyl.radius;
+        this.theta = cyl.theta;
+        this.y = cyl.y;
         return this;
+    }
+
+    setFromVector3(v) {
+        this.radius = Math.sqrt(v.x * v.x + v.z * v.z);
+        this.theta = Math.atan2(v.x, v.z);
+        this.y = v.y;
+        return this;
+    }
+
+    toVector3(target = new Vector3()) {
+        target.x = this.radius * Math.sin(this.theta);
+        target.y = this.y;
+        target.z = this.radius * Math.cos(this.theta);
+        return target;
+    }
+
+    toString() {
+        return `Cylindrical(radius: ${this.radius}, theta: ${this.theta}, y: ${this.y})`;
     }
 }
 
