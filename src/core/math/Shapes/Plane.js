02fddf80840d30369e8e5fe3a981bd764b52d759
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:18:58 +0700

    Update Plane.js

--- src/core/math/Shapes/Plane.js
+++ src/core/math/Shapes/Plane.js
@@ -1,1 +1,20 @@
+export class Plane {
+  constructor(normal = new Vector3(1, 0, 0), constant = 0) {
+    this.normal = normal;
+    this.constant = constant;
+  }
 
+  set(normal, constant) {
+    this.normal.copy(normal);
+    this.constant = constant;
+    return this;
+  }
+
+  clone() {
+    return new Plane(this.normal.clone(), this.constant);
+  }
+
+  distanceToPoint(point) {
+    return this.normal.dot(point) + this.constant;
+  }
+}
