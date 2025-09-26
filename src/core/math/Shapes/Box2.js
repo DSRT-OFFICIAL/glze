39f43a92f76648f7dac3ea6a4ced09d329f9f60f
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:30:24 +0700

    Update Box2.js

--- src/core/math/Shapes/Box2.js
+++ src/core/math/Shapes/Box2.js
@@ -1,5 +1,5 @@
 // core/math/Shapes/Box2.js
-import { Vector2 } from '../primitives/Vector2.js';
+import { Vector2 } from '../Vector2.js';
 
 class Box2 {
     constructor(min = new Vector2(+Infinity, +Infinity), max = new Vector2(-Infinity, -Infinity)) {
@@ -13,24 +13,14 @@ class Box2 {
         return this;
     }
 
-    setFromPoints(points) {
-        this.min.set(+Infinity, +Infinity);
-        this.max.set(-Infinity, -Infinity);
-        points.forEach(p => this.expandByPoint(p));
-        return this;
+    clone() {
+        return new Box2(this.min.clone(), this.max.clone());
     }
 
-    expandByPoint(point) {
-        this.min.x = Math.min(this.min.x, point.x);
-        this.min.y = Math.min(this.min.y, point.y);
-        this.max.x = Math.max(this.max.x, point.x);
-        this.max.y = Math.max(this.max.y, point.y);
+    copy(box) {
+        this.min.copy(box.min);
+        this.max.copy(box.max);
         return this;
-    }
-
-    containsPoint(point) {
-        return point.x >= this.min.x && point.x <= this.max.x &&
-               point.y >= this.min.y && point.y <= this.max.y;
     }
 
     getCenter(target = new Vector2()) {
@@ -41,18 +31,19 @@ class Box2 {
         return target.subVectors(this.max, this.min);
     }
 
-    isEmpty() {
-        return this.max.x < this.min.x || this.max.y < this.min.y;
+    containsPoint(point) {
+        return point.x >= this.min.x && point.x <= this.max.x &&
+               point.y >= this.min.y && point.y <= this.max.y;
     }
 
-    clone() {
-        return new Box2().copy(this);
-    }
-
-    copy(box) {
-        this.min.copy(box.min);
-        this.max.copy(box.max);
+    expandByPoint(point) {
+        this.min.min(point);
+        this.max.max(point);
         return this;
+    }
+
+    toString() {
+        return `Box2(min: ${this.min.toArray()}, max: ${this.max.toArray()})`;
     }
 }
 
