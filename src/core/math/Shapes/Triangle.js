3d9d32a0b0e23e47305c5eb8a81248fdb92cba39
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:18:39 +0700

    Update Triangle.js

--- src/core/math/Shapes/Triangle.js
+++ src/core/math/Shapes/Triangle.js
@@ -1,1 +1,18 @@
+export class Triangle {
+  constructor(a = new Vector3(), b = new Vector3(), c = new Vector3()) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
 
+  set(a, b, c) {
+    this.a.copy(a);
+    this.b.copy(b);
+    this.c.copy(c);
+    return this;
+  }
+
+  clone() {
+    return new Triangle(this.a.clone(), this.b.clone(), this.c.clone());
+  }
+}
