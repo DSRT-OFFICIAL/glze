21e4d13315b08eb76f9a2470cec7b2e1d2cd8a9a
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 21:18:41 +0700

    Update index.js

--- src/core/math/Primitives/index.js
+++ src/core/math/Primitives/index.js
@@ -1,7 +1,9 @@
-export { Vector2 } from './Vector2.js';
-export { Vector3 } from './Vector3.js';
-export { Vector4 } from './Vector4.js';
-export { Matrix2 } from './Matrix2.js';
-export { Matrix3 } from './Matrix3.js';
-export { Matrix4 } from './Matrix4.js';
-export { Quaternion } from './Quaternion.js';
+import { Vector2 } from './Vector2.js';
+import { Vector3 } from './Vector3.js';
+import { Vector4 } from './Vector4.js';
+import { Matrix2 } from './Matrix2.js';
+import { Matrix3 } from './Matrix3.js';
+import { Matrix4 } from './Matrix4.js';
+import { Quaternion } from './Quaternion.js';
+
+export { Vector2, Vector3, Vector4, Matrix2, Matrix3, Matrix4, Quaternion };
