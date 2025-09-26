975b3e8ce238b523a725177faca24dec2da74cfc
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:29:14 +0700

    Update Frustum.js

--- src/core/math/Shapes/Frustum.js
+++ src/core/math/Shapes/Frustum.js
@@ -1,27 +1,9 @@
 // core/math/Shapes/Frustum.js
 import { Plane } from './Plane.js';
-import { Vector3 } from '../primitives/Vector3.js';
 
 class Frustum {
     constructor(p0 = new Plane(), p1 = new Plane(), p2 = new Plane(), p3 = new Plane(), p4 = new Plane(), p5 = new Plane()) {
         this.planes = [p0, p1, p2, p3, p4, p5];
-    }
-
-    set(p0, p1, p2, p3, p4, p5) {
-        this.planes[0] = p0;
-        this.planes[1] = p1;
-        this.planes[2] = p2;
-        this.planes[3] = p3;
-        this.planes[4] = p4;
-        this.planes[5] = p5;
-        return this;
-    }
-
-    containsPoint(point) {
-        for (let i = 0; i < 6; i++) {
-            if (this.planes[i].distanceToPoint(point) < 0) return false;
-        }
-        return true;
     }
 
     clone() {
@@ -29,8 +11,38 @@ class Frustum {
     }
 
     copy(frustum) {
-        for (let i = 0; i < 6; i++) this.planes[i].copy(frustum.planes[i]);
+        this.planes.forEach((plane, i) => plane.copy(frustum.planes[i]));
         return this;
+    }
+
+    containsPoint(point) {
+        return this.planes.every(plane => plane.distanceToPoint(point) <= 0);
+    }
+
+    intersectsBox(box) {
+        return this.planes.some(plane => plane.intersectsBox(box));
+    }
+
+    intersectsSphere(sphere) {
+        return this.planes.some(plane => plane.intersectsSphere(sphere));
+    }
+
+    setFromProjectionMatrix(matrix) {
+        const me = matrix.elements;
+        const planes = this.planes;
+
+        planes[0].set(me[3] + me[0], me[7] + me[4], me[11] + me[8], me[15] + me[12]); // left
+        planes[1].set(me[3] - me[0], me[7] - me[4], me[11] - me[8], me[15] - me[12]); // right
+        planes[2].set(me[3] + me[1], me[7] + me[5], me[11] + me[9], me[15] + me[13]); // top
+        planes[3].set(me[3] - me[1], me[7] - me[5], me[11] - me[9], me[15] - me[13]); // bottom
+        planes[4].set(me[3] + me[2], me[7] + me[6], me[11] + me[10], me[15] + me[14]); // near
+        planes[5].set(me[3] - me[2], me[7] - me[6], me[11] - me[10], me[15] - me[14]); // far
+
+        return this;
+    }
+
+    toString() {
+        return `Frustum(planes: [${this.planes.map(plane => plane.toString())}])`;
     }
 }
 
