0d84a662b55d2dedd1c01a96270730131313bbd4
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 21:04:26 +0700

    Update Random.js

--- src/core/math/Utils/Random.js
+++ src/core/math/Utils/Random.js
@@ -1,1 +1,25 @@
+// glze/src/core/math/Random.js
 
+let _seed = 123456789;
+
+export function seed(s) {
+    _seed = s % 2147483647;
+    if (_seed <= 0) _seed += 2147483646;
+}
+
+export function random() {
+    _seed = (_seed * 16807) % 2147483647;
+    return (_seed - 1) / 2147483646;
+}
+
+export function randomRange(min, max) {
+    return min + (max - min) * random();
+}
+
+export function randomInt(min, max) {
+    return Math.floor(randomRange(min, max + 1));
+}
+
+export function randomChoice(array) {
+    return array[randomInt(0, array.length - 1)];
+}
