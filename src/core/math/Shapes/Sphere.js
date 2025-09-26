fbc96bf43e55e4a68b44767429579eae528fba0e
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:19:20 +0700

    Update Sphere.js

--- src/core/math/Shapes/Sphere.js
+++ src/core/math/Shapes/Sphere.js
@@ -1,1 +1,20 @@
+export class Sphere {
+  constructor(center = new Vector3(), radius = 1) {
+    this.center = center;
+    this.radius = radius;
+  }
 
+  set(center, radius) {
+    this.center.copy(center);
+    this.radius = radius;
+    return this;
+  }
+
+  clone() {
+    return new Sphere(this.center.clone(), this.radius);
+  }
+
+  containsPoint(point) {
+    return this.center.distanceTo(point) <= this.radius;
+  }
+}
