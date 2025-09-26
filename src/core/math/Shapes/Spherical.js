2f0f6dcda18c345eb840bc206a4f979f210713ef
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:27:57 +0700

    Update Spherical.js

--- src/core/math/Shapes/Spherical.js
+++ src/core/math/Shapes/Spherical.js
@@ -1,9 +1,11 @@
 // core/math/Shapes/Spherical.js
+import { Vector3 } from '../Vector3.js';
+
 class Spherical {
     constructor(radius = 1, phi = 0, theta = 0) {
-        this.radius = radius;
-        this.phi = phi;   // polar angle from y-axis
-        this.theta = theta; // azimuthal angle from z-axis
+        this.radius = radius; // distance from origin
+        this.phi = phi;       // polar angle from y-axis
+        this.theta = theta;   // azimuthal angle from z-axis
     }
 
     set(radius, phi, theta) {
@@ -14,7 +16,7 @@ class Spherical {
     }
 
     clone() {
-        return new Spherical().copy(this);
+        return new Spherical(this.radius, this.phi, this.theta);
     }
 
     copy(spherical) {
@@ -22,6 +24,30 @@ class Spherical {
         this.phi = spherical.phi;
         this.theta = spherical.theta;
         return this;
+    }
+
+    setFromVector3(v) {
+        this.radius = v.length();
+        if (this.radius === 0) {
+            this.theta = 0;
+            this.phi = 0;
+        } else {
+            this.theta = Math.atan2(v.x, v.z);
+            this.phi = Math.acos(Math.min(Math.max(v.y / this.radius, -1), 1));
+        }
+        return this;
+    }
+
+    toVector3(target = new Vector3()) {
+        const sinPhiRadius = Math.sin(this.phi) * this.radius;
+        target.x = sinPhiRadius * Math.sin(this.theta);
+        target.y = Math.cos(this.phi) * this.radius;
+        target.z = sinPhiRadius * Math.cos(this.theta);
+        return target;
+    }
+
+    toString() {
+        return `Spherical(radius: ${this.radius}, phi: ${this.phi}, theta: ${this.theta})`;
     }
 }
 
