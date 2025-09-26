94912a84829d8469bec87f3587d8443f7607e8af
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 20:01:48 +0700

    Update Sphere.js

--- src/core/math/Shapes/Sphere.js
+++ src/core/math/Shapes/Sphere.js
@@ -1,20 +1,43 @@
-export class Sphere {
-  constructor(center = new Vector3(), radius = 1) {
-    this.center = center;
-    this.radius = radius;
-  }
+// core/math/Shapes/Sphere.js
+import { Vector3 } from '../primitives/Vector3.js';
 
-  set(center, radius) {
-    this.center.copy(center);
-    this.radius = radius;
-    return this;
-  }
+class Sphere {
+    constructor(center = new Vector3(), radius = 0) {
+        this.center = center;
+        this.radius = radius;
+    }
 
-  clone() {
-    return new Sphere(this.center.clone(), this.radius);
-  }
+    set(center, radius) {
+        this.center.copy(center);
+        this.radius = radius;
+        return this;
+    }
 
-  containsPoint(point) {
-    return this.center.distanceTo(point) <= this.radius;
-  }
+    setFromPoints(points, optionalCenter) {
+        const center = optionalCenter || this.center.set(0,0,0);
+        if (!optionalCenter) points.forEach(p => center.add(p));
+        if (!optionalCenter) center.multiplyScalar(1 / points.length);
+
+        let maxRadiusSq = 0;
+        points.forEach(p => maxRadiusSq = Math.max(maxRadiusSq, center.distanceToSquared(p)));
+        this.radius = Math.sqrt(maxRadiusSq);
+
+        return this;
+    }
+
+    containsPoint(point) {
+        return point.distanceToSquared(this.center) <= (this.radius * this.radius);
+    }
+
+    clone() {
+        return new Sphere().copy(this);
+    }
+
+    copy(sphere) {
+        this.center.copy(sphere.center);
+        this.radius = sphere.radius;
+        return this;
+    }
 }
+
+export { Sphere };
