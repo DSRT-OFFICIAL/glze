cf2320a64206316250aa2c52543893f131be4a81
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:17:51 +0700

    Update Frustum.js

--- src/core/math/Shapes/Frustum.js
+++ src/core/math/Shapes/Frustum.js
@@ -1,1 +1,13 @@
+export class Frustum {
+  constructor(planes = []) {
+    this.planes = planes; // array of Plane
+  }
 
+  setFromMatrix(m) {
+    // implementasi sederhana mirip three.js
+    const planes = [];
+    for (let i = 0; i < 6; i++) planes[i] = new Plane();
+    this.planes = planes;
+    return this;
+  }
+}
