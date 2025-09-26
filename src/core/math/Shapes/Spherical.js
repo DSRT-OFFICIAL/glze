e1193cd6fa6386453c707e56d9e6279def8e8043
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:59:20 +0700

    Update Spherical.js

--- src/core/math/Shapes/Spherical.js
+++ src/core/math/Shapes/Spherical.js
@@ -1,18 +1,28 @@
-export class Spherical {
-  constructor(radius = 1, phi = 0, theta = 0) {
-    this.radius = radius;
-    this.phi = phi;     // polar angle
-    this.theta = theta; // azimuthal angle
-  }
+// core/math/Shapes/Spherical.js
+class Spherical {
+    constructor(radius = 1, phi = 0, theta = 0) {
+        this.radius = radius;
+        this.phi = phi;   // polar angle from y-axis
+        this.theta = theta; // azimuthal angle from z-axis
+    }
 
-  set(radius, phi, theta) {
-    this.radius = radius;
-    this.phi = phi;
-    this.theta = theta;
-    return this;
-  }
+    set(radius, phi, theta) {
+        this.radius = radius;
+        this.phi = phi;
+        this.theta = theta;
+        return this;
+    }
 
-  clone() {
-    return new Spherical(this.radius, this.phi, this.theta);
-  }
+    clone() {
+        return new Spherical().copy(this);
+    }
+
+    copy(spherical) {
+        this.radius = spherical.radius;
+        this.phi = spherical.phi;
+        this.theta = spherical.theta;
+        return this;
+    }
 }
+
+export { Spherical };
