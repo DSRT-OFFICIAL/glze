b244a6047b04700939ddf1e68e5623cbc163ec51
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:40:39 +0700

    Update SphericalHarmonics3.js

--- src/core/math/Shapes/SphericalHarmonics3.js
+++ src/core/math/Shapes/SphericalHarmonics3.js
@@ -1,16 +1,33 @@
-export class SphericalHarmonics3 {
-  constructor() {
-    this.coefficients = Array.from({ length: 9 }, () => new Vector3());
-  }
+// core/math/Shapes/SphericalHarmonics3.js
+class SphericalHarmonics3 {
+    constructor() {
+        this.coefficients = [];
+        for (let i = 0; i < 9; i++) this.coefficients.push([0,0,0]); // RGB triplets
+    }
 
-  add(sh) {
-    for (let i = 0; i < 9; i++) this.coefficients[i].add(sh.coefficients[i]);
-    return this;
-  }
+    setCoefficient(index, r, g, b) {
+        if (index < 0 || index > 8) throw new Error('Index out of range');
+        this.coefficients[index][0] = r;
+        this.coefficients[index][1] = g;
+        this.coefficients[index][2] = b;
+        return this;
+    }
 
-  clone() {
-    const newSH = new SphericalHarmonics3();
-    for (let i = 0; i < 9; i++) newSH.coefficients[i] = this.coefficients[i].clone();
-    return newSH;
-  }
+    getCoefficient(index) {
+        if (index < 0 || index > 8) throw new Error('Index out of range');
+        return this.coefficients[index];
+    }
+
+    clone() {
+        const sh = new SphericalHarmonics3();
+        for (let i = 0; i < 9; i++) sh.coefficients[i] = [...this.coefficients[i]];
+        return sh;
+    }
+
+    copy(sh) {
+        for (let i = 0; i < 9; i++) this.coefficients[i] = [...sh.coefficients[i]];
+        return this;
+    }
 }
+
+export { SphericalHarmonics3 };
