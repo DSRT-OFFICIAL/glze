153142e4b0b7aa66e22555ca94279583103eeecc
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:27:38 +0700

    Update FrustumArray.js

--- src/core/math/Shapes/FrustumArray.js
+++ src/core/math/Shapes/FrustumArray.js
@@ -1,16 +1,35 @@
-export class FrustumArray {
-  constructor() {
-    this.frustums = [];
-  }
+// core/math/Shapes/FrustumArray.js
+import { Frustum } from './Frustum.js';
 
-  add(frustum) {
-    this.frustums.push(frustum);
-    return this;
-  }
+class FrustumArray {
+    constructor() {
+        this.frustums = [];
+    }
 
-  clone() {
-    const newArr = new FrustumArray();
-    this.frustums.forEach(f => newArr.add(f.clone()));
-    return newArr;
-  }
+    add(frustum) {
+        this.frustums.push(frustum);
+        return this;
+    }
+
+    remove(index) {
+        this.frustums.splice(index, 1);
+        return this;
+    }
+
+    containsPoint(point) {
+        return this.frustums.some(f => f.containsPoint(point));
+    }
+
+    clone() {
+        const arr = new FrustumArray();
+        this.frustums.forEach(f => arr.add(f.clone()));
+        return arr;
+    }
+
+    copy(array) {
+        this.frustums = array.frustums.map(f => f.clone());
+        return this;
+    }
 }
+
+export { FrustumArray };
