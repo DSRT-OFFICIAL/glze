9c1bb6d4239ad47bc0251901791d15e11a48fd90
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:16:34 +0700

    Update SphericalHarmonics3.js

--- src/core/math/Shapes/SphericalHarmonics3.js
+++ src/core/math/Shapes/SphericalHarmonics3.js
@@ -1,1 +1,16 @@
+export class SphericalHarmonics3 {
+  constructor() {
+    this.coefficients = Array.from({ length: 9 }, () => new Vector3());
+  }
 
+  add(sh) {
+    for (let i = 0; i < 9; i++) this.coefficients[i].add(sh.coefficients[i]);
+    return this;
+  }
+
+  clone() {
+    const newSH = new SphericalHarmonics3();
+    for (let i = 0; i < 9; i++) newSH.coefficients[i] = this.coefficients[i].clone();
+    return newSH;
+  }
+}
