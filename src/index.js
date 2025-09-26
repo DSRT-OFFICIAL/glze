24318bd95ba832c048a1835680b223a31f3822f9
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 22:37:41 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,81 @@
+// glze/src/index.js
 
+// =========================
+// Core Math
+// =========================
+export * from './core/math/index.js';
+
+// =========================
+// Core Animation
+// =========================
+export * from './core/animation/index.js';
+
+// =========================
+// Core Cameras
+// =========================
+export * from './core/cameras/index.js';
+
+// =========================
+// Core Geometry
+// =========================
+export * from './core/geometry/index.js';
+
+// =========================
+// Core Lights
+// =========================
+export * from './core/lights/index.js';
+
+// =========================
+// Core Loaders
+// =========================
+export * from './core/loaders/index.js';
+
+// =========================
+// Core Materials
+// =========================
+export * from './core/materials/index.js';
+
+// =========================
+// Core Nodes
+// =========================
+export * from './core/nodes/index.js';
+
+// =========================
+// Core Renderer
+// =========================
+export * from './core/renderer/index.js';
+
+// =========================
+// Core Scene
+// =========================
+export * from './core/scene/index.js';
+
+// =========================
+// Core Textures
+// =========================
+export * from './core/textures/index.js';
+
+// =========================
+// Core Helpers
+// =========================
+export * from './core/helpers/index.js';
+
+// =========================
+// Services
+// =========================
+export * from './services/index.js';
+
+// =========================
+// Extras
+// =========================
+export * from './extras/index.js';
+
+// =========================
+// Game (opsional)
+// =========================
+export * from './game/index.js';
+
+// =========================
+// Metaverse (opsional)
+// =========================
+export * from './metaverse/index.js';
