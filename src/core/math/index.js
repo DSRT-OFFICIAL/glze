3c7ead5a8dede4fb535a06e255a4c7e41db6dba9
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 18:17:22 +0700

    Update index.js

--- src/core/math/index.js
+++ src/core/math/index.js
@@ -1,1 +1,18 @@
+// ===========================
+// Glaze Math Central Export
+// ===========================
 
+// Primitives
+export * from './Primitives/index.js'; // Vector2/3/4, Matrix2/3/4, Quaternion
+
+// Shapes
+export * from './Shapes/index.js'; // Box2/3, Sphere, Plane, Triangle, Frustum, FrustumArray, Spherical, Cylindrical, SphericalHarmonics3
+
+// Interpolants
+export * from './Interpolant/index.js'; // Interpolant, LinearInterpolant, CubicInterpolant, DiscreteInterpolant, QuaternionLinearInterpolant
+
+// Utils
+export * from './Utils/index.js'; // GlazeMathUtils, MathUtils, Random, Interpolation, Easing, Constants
+
+// Color
+export * from './Color/index.js'; // Color class, BasicGlazeColors, ExtendedGlazeColors
