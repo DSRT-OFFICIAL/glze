543a4a7a13fdc2fbc722d0ab27b961425ac5a87d
DSRT-OFFICIAL <sxcdir@gmail.com>
2025-09-26 19:17:32 +0700

    Update FrustumArray.js

--- src/core/math/Shapes/FrustumArray.js
+++ src/core/math/Shapes/FrustumArray.js
@@ -1,1 +1,16 @@
+export class FrustumArray {
+  constructor() {
+    this.frustums = [];
+  }
 
+  add(frustum) {
+    this.frustums.push(frustum);
+    return this;
+  }
+
+  clone() {
+    const newArr = new FrustumArray();
+    this.frustums.forEach(f => newArr.add(f.clone()));
+    return newArr;
+  }
+}
